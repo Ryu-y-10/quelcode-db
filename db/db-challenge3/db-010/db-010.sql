54fe1e5b2dbde29a2a9e7ef373165db7254db7a6
Ryu-y-10 <ryu@RyunosukenoMacBook-Pro.local>
2020-07-28 15:11:58 +0900

    fix:SQL文の修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,4 +1,4 @@
-SELECT user_name,posted_at,room_name FROM users u,chat_rooms r,post_contents p1
-WHERE posted_at = (SELECT MAX(posted_at) FROM post_contents p2 WHERE p1.room_id = p2.room_id) 
-AND p1.posted_by = u.user_id AND r.room_id = p1.room_id 
-AND u.is_deleted = 0 AND p1.is_deleted = 0 ORDER BY p1.room_id ASC;
+SELECT user_name,p1.posted_at,room_name FROM users u,post_contents p1,chat_rooms r,
+(SELECT MAX(p2.posted_at) AS new_posted_at,p2.room_id FROM post_contents p2 WHERE p2.is_deleted = 0 GROUP BY p2.room_id) p3 
+WHERE p1.posted_by = user_id AND r.room_id = p1.room_id AND u.is_deleted = 0 AND p1.posted_at = p3.new_posted_at 
+ORDER BY p1.room_id ASC
