6d3106c07f1286a8c73b2a6375d7600167ab6a02
Ryu-y-10 <ryu@RyunosukenoMacBook-Pro.local>
2020-07-28 12:21:18 +0900

    fix:不必要なWHERE句の削除

--- db/db-challenge3/db-006/db-006.sql
+++ db/db-challenge3/db-006/db-006.sql
@@ -1,2 +1,2 @@
 SELECT user_id,user_name,email_address,password,profile,work_phone_number,personal_mobile_number,is_deleted,created_at,updated_at FROM users 
-WHERE is_deleted IN(0,1) ORDER BY user_id ASC;
+ORDER BY user_id ASC;
